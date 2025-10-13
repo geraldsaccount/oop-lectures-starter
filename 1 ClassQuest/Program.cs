f0a0287d275bfb36e2e3ed88701a8d25c1f36cad
gerald <geraldmontufar@gmail.com>
2025-10-13 15:38:27 +0200

    removed unneeded scripts

--- 1 ClassQuest/Program.cs
+++ 1 ClassQuest/Program.cs
@@ -1,22 +1,11 @@
 ﻿using _1_ClassQuest.Weapons;
 
-namespace _1_ClassQuest {
-    internal class Program {
-        private static void Main(string[] args) {
-            Character player = new Paladin("Gerald", 100, 20);
-            IWeapon sword = new Sword("Excalibur", 20);
-            Character enemy = new Rogue("John", 100, 0.4f, 1.2f);
-            IWeapon dagger = new Dagger("Butter Knife", 30, 0.5f);
-
-            player.EquipWeapon(sword);
-            enemy.EquipWeapon(dagger);
-
-            while (player.Health > 0 && enemy.Health > 0) {
-                player.Attack(enemy);
-                enemy.Attack(player);
-                Console.ReadKey(true);
-                Console.Clear();
-            }
+namespace _1_ClassQuest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
         }
     }
 }
