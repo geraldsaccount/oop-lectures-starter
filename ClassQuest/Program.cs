5d6149ba26e47c2482d3ca33e8a8537d62e2b183
geraldsaccount <geraldmontufar@gmail.com>
2025-10-26 11:47:21 +0100

    added simple combat logic

--- ClassQuest/Program.cs
+++ ClassQuest/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Weapon sword = new Weapon("Long Sword", 25);
+        Weapon axe = new Weapon("Battle Axe", 30);
+
+        Player hero = new Player("Aragorn", 100, new int[] { 0, 0 }, sword);
+        Enemy goblin = new Enemy("Goblin", 75, new int[] { 1, 0 }, axe);
+
+        hero.Attack(goblin);
+        goblin.Attack(hero);
     }
 }
